306ea9ca6aa9d1978ccb96c3843ede5e87c617eb
Aeroshide <78432778+Aeroshide@users.noreply.github.com>
2023-06-28 19:51:39 +0700

    job is gay

--- src/main/java/com/aeroshide/efficient/mixins/LoadingScreenRendererMixin.java
+++ src/main/java/com/aeroshide/efficient/mixins/LoadingScreenRendererMixin.java
@@ -4,7 +4,6 @@ import com.aeroshide.efficient.Efficient;
 import net.minecraft.client.gui.WorldGenerationProgressTracker;
 import net.minecraft.client.gui.screen.LevelLoadingScreen;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,11 +22,11 @@ public class LoadingScreenRendererMixin {
     @Shadow
     private WorldGenerationProgressTracker progressProvider;
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/LevelLoadingScreen;drawCenteredString(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V"))
-    private void modifyProgressString(LevelLoadingScreen levelLoadingScreen, MatrixStack matrixStack, TextRenderer textRenderer, String string, int x, int y, int color) {
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/LevelLoadingScreen;drawCenteredString(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V"))
+    private void modifyProgressString(LevelLoadingScreen levelLoadingScreen, TextRenderer textRenderer, String string, int x, int y, int color) {
         String originalString = Efficient.df.format(MathHelper.clamp(this.progressProvider.getProgressPercentage() * 3.9, 0, 100)) + "%";
         String modifiedString = "ACCELERATED: " + originalString;
-        levelLoadingScreen.drawCenteredString(matrixStack ,textRenderer, modifiedString, x, y, color);
+        levelLoadingScreen.drawCenteredString(textRenderer, modifiedString, x, y, color);
     }
 
     /*
